3eda95b69fc138564c26c4c6c58377e7d0299ad8
Magnus Larsson <magnus.larsson@callistaenterprise.se>
2023-04-07 16:31:33 +0200

    Chapter12

---
+++ Chapter12/spring-cloud/gateway/src/test/java/se/magnus/springcloud/gateway/GatewayApplicationTests.java
@@ -0,0 +1,18 @@
+package se.magnus.springcloud.gateway;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT,
+  properties = {
+    "eureka.client.enabled=false",
+    "spring.cloud.config.enabled=false"})
+class GatewayApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
