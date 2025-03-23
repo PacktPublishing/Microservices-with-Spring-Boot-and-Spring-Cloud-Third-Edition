ff99ed7fbf504ab015bbbc6c4723c3824a6a6c50
isakatirci <ce.isakatirci@gmail.com>
2025-03-23 22:30:15 +0300

    Spring Cloud Gateway Update

--- Chapter09/microservices/product-composite-service/src/main/java/se/magnus/microservices/composite/product/ProductCompositeServiceApplication.java
+++ Chapter09/microservices/product-composite-service/src/main/java/se/magnus/microservices/composite/product/ProductCompositeServiceApplication.java
@@ -78,7 +78,6 @@ public class ProductCompositeServiceApplication {
     return Schedulers.newBoundedElastic(threadPoolSize, taskQueueSize, "publish-pool");
   }
 
-
   @Bean
   @LoadBalanced
   public WebClient.Builder loadBalancedWebClientBuilder() {
